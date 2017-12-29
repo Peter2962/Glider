61cd9a0a5f463018a972f2339d22faeb3cb6e373
PhoxPHP <taiwopeter35@gmail.com>
2017-12-29 02:22:25 +0200

    Updated composer and configuration

---
+++ Config.php
@@ -0,0 +1,31 @@
+<?php
+return [
+	'default' => [
+		'provider' => Glider\Platform\Mysqli\MysqliProvider::class,
+		'host' => 'localhost',
+		'alias' => 'mysqli',
+		'username' => 'root',
+		'password' => 'root',
+		'database' => 'test',
+		'charset' => 'utf8',
+		'collation' => '',
+		'domain' => 'glider.site',
+		'auto_commit' => false,
+		'prefix' => '',
+		'alt' => 'dev'
+	],
+	'dev' => [
+		'provider' => Glider\Platform\Pdo\PdoProvider::class,
+		'host' => 'localhost',
+		'alias' => 'pdo',
+		'username' => 'root',
+		'password' => 'root',
+		'database' => 'test',
+		'charset' => 'utf8',
+		'collation' => 'utf8',
+		'domain' => 'http://server.web/',
+		'prefix' => '',
+		'auto_commit' => true,
+		'alt' => null
+	]
+];
