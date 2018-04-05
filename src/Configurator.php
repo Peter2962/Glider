04c1c963aac6b93074652489da442ef94394cf1b
Peter2962 <taiwop3@gmail.com>
2018-04-06 00:46:33 +0200

    Fixing configuration files path

--- src/Configurator.php
+++ src/Configurator.php
@@ -47,7 +47,7 @@ class Configurator implements ConfiguratorInterface
 			unlink('Config.php');
 		}
 
-		rename('/app/vendor/phoxphp/glider/src/config.framework', '/app/vendor/phoxphp/glider/src/Config.php');
+		copy('app/vendor/phoxphp/glider/src/config.framework', 'app/vendor/phoxphp/glider/src/Config.php');
 	}
 
 }
